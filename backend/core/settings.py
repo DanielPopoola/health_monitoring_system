cfa4db6f99a6eb10ed56b0011ba287251e7f1d91
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-04-16 19:00:56 +0100

    minor change

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env("DEBUG")
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = []
 
 
 # Application definition
