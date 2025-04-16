84ff03d8c7d4cd3aa4809b12e5af8921f547fc7b
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-04-16 19:33:14 +0100

    added permission classes and improved error handling

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,11 +1,23 @@
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 class Register(APIView):
+
+    permission_classes = [AllowAny]
+
     def post(self, request):
         serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(
+                {
+                    "user":serializer.data,
+                    "message": "User registered successfully",
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
