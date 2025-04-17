7dbe9f3e27f54dd3a99ac54c325e7f3b32b0f326
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-04-17 18:43:25 +0100

    defined heart rate class

--- backend/health_metrics/models/heart_rate.py
+++ backend/health_metrics/models/heart_rate.py
@@ -1,6 +1,7 @@
 from .base import HealthMetric
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.db.models import Avg
 
 
 class HeartRate(HealthMetric):
@@ -21,5 +22,31 @@ class HeartRate(HealthMetric):
     def clean(self):
         super().clean()
         if not (30 <= self.value <= 220):
-            raise ValidationError("Heart ")
+            raise ValidationError("Heart rate must be between 30 and 220 BPM")
+        
+    def is_within_normal_range(self):
+        return 60 <= self.value <= 100
+    
+    @property
+    def heart_rate_zone(self):
+        if self.value < 60:
+            return 'Below Normal'
+        elif self.value <= 100:
+            return 'Normal'
+        elif self.value <= 140:
+            return 'Fat Burn'
+        elif self.value <= 170:
+            return 'Cardio'
+        else:
+            return 'Peak'
 
+    @property
+    def is_tarchycardia(self):
+        return self.value > 100
+    
+    @property
+    def is_bardycardia(self):
+        return self.value < 60
+    
+    def get_resting_average(self):
+        return HeartRate.objects.filter(user=self.user, activity_level='resting').aaggregate(avg=Avg('value'))['avg']
