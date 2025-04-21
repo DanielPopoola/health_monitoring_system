8d55910edee392825f2388faa7a3ad2e8ab572a0
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-04-21 17:47:24 +0100

    updated url pattern

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api/', include('health_metrics.urls')),
     path('api/', include('users.urls')),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh_view'),
