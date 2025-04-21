6038b4b5e0d17bf3fd520cde7497b3c8d28c5740
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-04-21 01:36:42 +0100

    added admin customization for user login

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
 
 # Register your models here.
+@admin.register(UserProfile)
+class CustomUserAdmin(UserAdmin):
+    model = UserProfile
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email', )
+
+    fieldsets = (
+        (None, {'fields':('email', 'password')}),
+        ('Personal info', {'fields': ('name', 'first_name', 'last_name', 'age', 'gender')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_superuser'),
+        }),
+    )
+
+    filter_horizontal = ('groups', 'user_permissions')
