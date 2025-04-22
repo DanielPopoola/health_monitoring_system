49f16487768d5f1739113f544a1edfef166db98e
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-04-22 18:36:26 +0100

    connect user registration to simulation setup

---
+++ backend/users/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import UserProfile
+from data_simulation.models import SimulationConfig
+
+@receiver(post_save, sender=UserProfile)
+def create_simulation_config(sender, instance, created, **kwargs):
+    if created:
+        SimulationConfig.create_for_user(instance)
