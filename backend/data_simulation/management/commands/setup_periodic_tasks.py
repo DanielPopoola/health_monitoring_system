4e5001264ddb950045fd0e440ada957b16e158d8
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-04-23 22:48:52 +0100

    redefined setup_periodic_tasks to delete previoulsy defined tasks and recreate them with fresh schedule bindings

--- backend/data_simulation/management/commands/setup_periodic_tasks.py
+++ backend/data_simulation/management/commands/setup_periodic_tasks.py
@@ -5,14 +5,23 @@ class Command(BaseCommand):
     help = 'Setup periodic tasks for data simulation'
 
     def handle(self, *args, **options):
-        # Create scheduls
+        # Clean up previously created tasks
+        task_names = [
+            'Generate Heart Rate',
+            'Generate Blood Pressure',
+            'Generate Oxygen Levels',
+            'Generate Daily Metrics',
+        ]
+        PeriodicTask.objects.filter(name__in=task_names).delete()
+
+        # Create schedules
         heart_schedule, _ = IntervalSchedule.objects.get_or_create(
-            every=60, # seconds
+            every=60,
             period=IntervalSchedule.SECONDS,
         )
 
         bp_schedule, _ = IntervalSchedule.objects.get_or_create(
-            every=300, # 5 minutes
+            every=300,
             period=IntervalSchedule.SECONDS,
         )
 
@@ -29,29 +38,29 @@ class Command(BaseCommand):
             month_of_year='*',
         )
 
-        # Create tasks
-        PeriodicTask.objects.get_or_create(
+        # Recreate tasks
+        PeriodicTask.objects.create(
             interval=heart_schedule,
             name='Generate Heart Rate',
             task='data_simulation.tasks.generate_heart_rate_for_all_users',
         )
 
-        PeriodicTask.objects.get_or_create(
+        PeriodicTask.objects.create(
             interval=bp_schedule,
             name='Generate Blood Pressure',
             task='data_simulation.tasks.generate_blood_pressure_for_all_users'
         )
 
-        PeriodicTask.objects.get_or_create(
+        PeriodicTask.objects.create(
             interval=sp_schedule,
             name='Generate Oxygen Levels',
             task='data_simulation.tasks.generate_spo2_for_all_users',
         )
 
-        PeriodicTask.objects.get_or_create(
+        PeriodicTask.objects.create(
             crontab=daily_schedule,
             name='Generate Daily Metrics',
             task='data_simulation.tasks.generate_daily_metrics_for_all_users',
         )
 
-        self.stdout.write(self.style.SUCCESS('Successfully set up periodic tasks'))
+        self.stdout.write(self.style.SUCCESS('Successfully reset periodic tasks'))
