cf684f3aa0cc68d398e18616f2d01a4ac2fbd187
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-04-28 13:29:34 +0100

    fixed role checking

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -9,4 +9,4 @@ class IsDoctorOrNurseOrAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         if not request.user or not request.user.is_authenticated:
             return False
-        return request.user.role
+        return request.user.role in ["DOCTOR", "NURSE", "ADMIN"]
